09ef7aea0cff0077ddad65debf7f57568b6041bd
the creator <tcreat0r1337@gmail.com>
2024-12-14 19:22:06 +0100

    a

---
+++ _next/static/pBBLKsjazVEjY-dC2jElA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/8d9b9ea7b69f1629.css","static/chunks/pages/index-55458fc639623016.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
